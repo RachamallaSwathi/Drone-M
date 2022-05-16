88d0816cda907d339b15fb8e3ac374279f371381
RachamallaSwathi <swathirachamalla77@gmail.com>
2022-05-16 17:08:34 +0530

    m-drone

---
+++ m-drone/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+//import Client from './components/Client';
+//import Contact from './components/Contact';
+//import Drone from './components/Drone';
+// import Footer from './components/Footer';
+import Header from './components/Header';
+//import Hero from './components/Hero';
+//import News from './components/News';
+//import Products from './components/Products';
+//import Testimonial from './components/Testimonial';
+
+function App() {
+  return (
+    <>
+    <Header />
+
+    
+    
+    
+    
+    
+    </>
+  );
+}
+
+export default App;
